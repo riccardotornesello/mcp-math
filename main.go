991adc40fb418eb390310cb1cabba89456191fe9
Riccardo Tornesello <riccardo.tornesello@gmail.com>
2025-11-14 09:57:08 +0100

    serve mcp via http

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/modelcontextprotocol/go-sdk/mcp"
 	"riccardotornesello.it/mcp-math/tools"
@@ -14,9 +13,14 @@ func main() {
 
 	mcp.AddTool(server, &mcp.Tool{Name: "sum", Description: "adds two numbers"}, tools.Sum)
 
-	fmt.Println("Starting MCP Math server...")
-	if err := server.Run(context.Background(), &mcp.StdioTransport{}); err != nil {
+	handler := mcp.NewStreamableHTTPHandler(
+		func(*http.Request) *mcp.Server { return server },
+		&mcp.StreamableHTTPOptions{},
+	)
+	http.HandleFunc("/mcp", handler.ServeHTTP)
+
+	log.Println("Starting MCP math server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Server stopped.")
 }
